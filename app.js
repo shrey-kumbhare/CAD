9a18412a00d0fc938c024bd69de5c979076292f1
shrey <shrey23kumbhare@gmail.com>
2023-06-01 15:25:02 +0530

    google o auth for patient only

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //jshint esversion:6
+require('dotenv').config()
+const dotenv=require('dotenv');
 require('mongodb')
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
+const passport = require("passport");
+const passportLocalMongoose  = require("passport-local-mongoose");
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var session = require('express-session')
+const findOrCreate = require("mongoose-findorcreate");
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -12,23 +19,64 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set('strictQuery', true); mongoose.connect('mongodb://127.0.0.1/cadDB');
 mongoose.set('strictQuery', false);
 
+
+
 const patientSchema = new mongoose.Schema ({
   email: String,
   password: String,
+  googleId:String
 });
+patientSchema.plugin(passportLocalMongoose);
+patientSchema.plugin(findOrCreate);
 const PATIENT =  mongoose.model("Patient", patientSchema);
 const proffesionalSchema = new mongoose.Schema ({
   email: String,
-  password: String,
+  password: String
 });
 const PROFFESIONAL =  mongoose.model("Proffesional", proffesionalSchema);
 app.get("/", function(req, res){
   res.render("home");
 });
+
+
+
+passport.use(PATIENT.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  PATIENT.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile)
+  PATIENT.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -103,6 +151,15 @@ app.get("/logout", function(req, res){
   res.redirect("/");
 });
 
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/PATIENTlogin" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/");
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
